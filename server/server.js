788eb3b6c91b0fda76d9ac4b8f876e0d90b32553
Joseph Sandmeyer <joey.sandmeyer@gmail.com>
2020-02-11 14:59:21 -0800

    Copy gal's proxy

---
+++ server/server.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+const port = 3003
+const path = require('path');
+const axios = require('axios');
+
+// Air Carousel
+app.get(
+  '/api/:house',
+  (req, res) => {
+    axios({
+      method: 'GET',
+      url: `http://localhost:1337` + req.url,
+    })
+      .then((innerRes) => {
+        console.log(innerRes.data);
+        res.writeHead(200);
+        res.write(JSON.stringify(innerRes.data))
+        res.end()
+      })
+      .catch((err) => {
+        console.log(err);
+        res.writeHead(500);
+        res.end()
+      });
+  })
+  
+// Reviews
+app.get(
+  '/v1/api/:house/reviews',
+  (req, res) => {
+    axios({
+      method: 'GET',
+      url: `http://localhost:2020` + req.url,
+    })
+      .then((innerRes) => {
+        console.log(innerRes.data);
+        res.writeHead(200);
+        res.write(JSON.stringify(innerRes.data))
+        res.end()
+      })
+      .catch((err) => {
+        console.log(err);
+        res.writeHead(500);
+        res.end()
+      });
+  })
+
+// Scheduling
+app.get(
+  '/api/v1/listings',
+  (req, res) => {
+    axios({
+      method: 'GET',
+      url: `http://localhost:3000` + req.url,
+    })
+      .then((innerRes) => {
+        console.log(innerRes.data);
+        res.writeHead(200);
+        res.write(JSON.stringify(innerRes.data))
+        res.end()
+      })
+      .catch((err) => {
+        console.log(err);
+        res.writeHead(500);
+        res.end()
+      });
+  })
+
+// Related Homes
+app.get(
+  '/gethomes',
+  (req, res) => {
+    axios({
+      method: 'GET',
+      url: `http://localhost:4321` + req.url,
+    })
+      .then((innerRes) => {
+        console.log(innerRes.data);
+        res.writeHead(200);
+        res.write(JSON.stringify(innerRes.data))
+        res.end()
+      })
+      .catch((err) => {
+        console.log(err);
+        res.writeHead(500);
+        res.end()
+      });
+  })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(express.static(path.join(__dirname, '../public')));
